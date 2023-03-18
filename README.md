05a31233d83b1860a4eea125d1e8981c35d284cc
kristentsuboi <kristenatsuboi@gmail.com>
2023-03-17 19:43:36 -0500

    completed project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MakeChangeProject
 
-##Overview
+## Overview
 
 ## Technologies Used
 - Java
@@ -8,6 +8,16 @@
 - Git
 
 ## Lessons Learned
+- Opened scanner to anticipate input from the user.
+- Asked user for input of and stored input of price and cash rendered.
+- If statement to check if exact change is provided, if so print message.
+- Else if to see if price > cash, if so print error message. 
+- Else statement to calculate change. Math.round to assist with rounding.
+
+
+
+
+
 gained understanding on this or that
 learned about converting this or that
 practiced using scanner to input with the user.
--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,8 +1,63 @@
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		System.out.println("FML");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the cash register!");
+		System.out.println("Please enter the price of the item: ");
+		double price = sc.nextDouble();
+		System.out.println("Please enter how much cash was rendered from the customer: ");
+		double cash = sc.nextDouble();
+
+		if (price == cash) {
+			System.out.println("The customer provided exact cash, no change required. ");
+		} else if (price >= cash) {
+			System.out.println("Customer didn't provide enough cash.");
+		} else {
+			double change = price - cash;
+			change = change * -1;
+			change = Math.round(change * 100.0) / 100.0;
+
+			System.out.println("CHANGE DUE: " + change);
+			int twenty = (int) Math.floor(change / 20);
+			int ten = (int) Math.floor((change - twenty * 20) / 10);
+			int five = (int) Math.floor((change - twenty * 20 - ten * 10) / 5);
+			int one = (int) Math.floor((change - twenty * 20 - ten * 10 - five * 5) / 1);
+			int quarter = (int) Math.floor((change - twenty * 20 - ten * 10 - five * 5 - one * 1) / 0.25);
+			int dime = (int) Math
+					.floor((change - twenty * 20 - ten * 10 - five * 5 - one * 1 - quarter * 0.25) / .10);
+			int nickel = (int) Math
+					.floor((change - twenty * 20 - ten * 10 - five * 5 - one * 1 - quarter * 0.25 - dime * 0.10) / .05);
+			int penny = (int) Math.round(
+					(change - twenty * 20 - ten * 10 - five * 5 - one * 1 - quarter * 0.25 - dime * .10 - nickel * 0.05)
+							/ 0.01);
+			if (twenty != 0) {
+				System.out.println("Twenty dollar bills: " + twenty);
+			}
+			if (ten != 0) {
+				System.out.println("Ten dollar bills: " + ten);
+			}
+			if (five != 0) {
+				System.out.println("Five dollar bills: " + five);
+			}
+			if (one != 0) {
+				System.out.println("One dollar bills: " + one);
+			}
+			if (quarter != 0) {
+				System.out.println("Quarters: " + quarter);
+			}
+			if (dime != 0) {
+				System.out.println("Dimes: " + dime);
+			}
+			if (nickel != 0) {
+				System.out.println("Nickels: " + nickel);
+			}
+			if (penny != 0) {
+				System.out.println("Pennies: " + penny);
+			}
+
+		}
 
 	}
 
